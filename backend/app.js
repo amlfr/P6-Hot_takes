1d367579b3db45b56c8066a57a01149ec3dcd402
amlfr <amartinez.lalenec@gmail.com>
2022-12-13 22:40:56 +0100

    user routes

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauces')
+
+mongoose.connect('mongodb+srv://amlfr:8D3JogBCGhDi7AkI@cluster0.lddpcur.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
