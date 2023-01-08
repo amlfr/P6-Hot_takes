f9a6cee31de4794ac856634f8275e686ff51d663
amlfr <amartinez.lalenec@gmail.com>
2023-01-08 14:55:50 +0100

    fixing like function

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,33 +1,29 @@
 /* Importing multer */
 
-const multer = require('multer');
+const multer = require("multer");
 
 /* Setting up the MIME types for the images */
 
 const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png",
 };
 
 /* Setting up the destination and naming convention of the files uploaded */
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, 'images')
+        callback(null, "images");
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        console.log('name ', name);
-        const nameCut = name.split('.');
-        console.log('nameCut ', nameCut);
-        
+        const name = file.originalname.split(" ").join("_");
+        const nameCut = name.split(".");
         const extension = MIME_TYPES[file.mimetype];
-        console.log('extension ', extension);
-        callback(null, nameCut[0] + Date.now() + '.' + extension);
-    }
+        callback(null, nameCut[0] + Date.now() + "." + extension);
+    },
 });
 
 /* Exporting the multer middleware */
 
-module.exports = multer({ storage }).single('image');
+module.exports = multer({ storage }).single("image");
